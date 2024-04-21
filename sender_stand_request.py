f0d2c2d7b5a36db563f895ddcfbbb593d9240c4e
lesya-86 <165170403+lesya-86@users.noreply.github.com>
2024-04-21 21:50:11 +0700

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,10 +5,7 @@ import data
 def post_new_order():
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_NEW_ORDER,
                          json=data.order_body)
-def get_info_track(track_id):
-    get_info_url = f"{configuration.URL_SERVICE}/api/v1/orders/track?t={track_id}"
-    response = requests.get(get_info_url)
-    return response
-    return requests.get(configuration.URL_SERVICE + configuration.GET_INFO_TRACK + str(track_id))
+def get_orders_track(track_id):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_INFO_BY_TRACK + str(track_id))
 
 
