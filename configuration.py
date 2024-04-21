c990fa7ff5da3869db7589ec3ed7b45eeb93af2e
lesya-86 <165170403+lesya-86@users.noreply.github.com>
2024-04-21 21:49:31 +0700

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
-URL_SERVICE = "https://cb943978-f770-44d0-b2a6-0c23db39d5d0.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://fb683dcb-98e8-484d-a139-ded007c4f031.serverhub.praktikum-services.ru"
 CREATE_NEW_ORDER = "/api/v1/orders"
 GET_INFO_TRACK = "/api/v1/orders/track"
+GET_INFO_BY_TRACK = "/api/v1/orders/track?t="
