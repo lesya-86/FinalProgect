462af3cd32edf71fad8f18362807c5a085b2602a
lesya-86 <165170403+lesya-86@users.noreply.github.com>
2024-04-21 14:23:19 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import data
 
 
 def test_order_creation():
-    new_response = sender_stand_request.post_new_order(data.order_body)
+    new_response = sender_stand_request.post_new_order()
     track_id = new_response.json()['track']
     response = sender_stand_request.get_info_track(track_id)
     assert response.status_code == 200
